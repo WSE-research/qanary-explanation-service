b2e6c8678b1fe886d898971a44d90dbcafba999a
dschiese <schiesedennis@googlemail.com>
2023-08-04 08:08:03 +0200

    Initial Spring

---
+++ src/main/java/com/wse/webservice_for_annotationsRequest/WebserviceForAnnotationsRequestApplication.java
@@ -0,0 +1,13 @@
+package com.wse.webservice_for_annotationsRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceForAnnotationsRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceForAnnotationsRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wse/webservice_for_annotationsRequest/WebserviceForAnnotationsRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wse.webservice_for_annotationsRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceForAnnotationsRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
