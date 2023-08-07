df51cbcdb1ff4b1a21406bd056dcb07620fbb2db
dschiese <schiesedennis@googlemail.com>
2023-08-07 13:40:11 +0200

    Frontend to display explanation data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Explanation from "./explanationComponent/Explanation";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    R
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Explanation />
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Explanation />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
