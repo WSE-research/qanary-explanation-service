3da147b536ee58a4812e5681dff34831236a1c58
dschiese <schiesedennis@googlemail.com>
2023-08-07 11:57:27 +0200

    frontend for displaying data

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Explanation from "./explanationComponent/Explanation";
+
+function App() {
+  return (
+    <div className="App">
+      <Explanation />
+    </div>
+  );
+}
+
+export default App;
