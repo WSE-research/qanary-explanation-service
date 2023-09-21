85953228216e025c809c57655fa6fb8b00834410
Dennis Schiese <schiesedennis@googlemail.com>
2023-09-21 15:40:58 +0200

    Added support for annotation type AnnotationOfQuestionTranslation

--- src/main/java/com/wse/qanaryexplanationservice/services/ExplanationService.java
+++ src/main/java/com/wse/qanaryexplanationservice/services/ExplanationService.java
@@ -58,6 +58,7 @@ public class ExplanationService {
         put("annotationofanswersparql", "/queries/queries_for_annotation_types/annotations_of_answer_sparql.rq");
         put("annotationofrelation", "/queries/queries_for_annotation_types/annotations_of_relation_query.rq");
         put("annotationofanswerjson", "/queries/queries_for_annotation_types/annotations_of_answer_json_query.rq");
+        put("annotationofquestiontranslation", "/queries/queries_for_annotation_types/annotations_of_question_translation_query.rq");
     }};
 
     // Holds explanation templates for the declared annotation types
@@ -67,6 +68,7 @@ public class ExplanationService {
         put("annotationofanswersparql", "/explanations/annotation_of_answer_sparql/");
         put("annotationofrelation", "/explanations/annotation_of_relation/");
         put("annotationofanswerjson", "/explanations/annotation_of_answer_json/");
+        put("annotationofquestiontranslation", "/explanations/annotation_of_question_translation/");
     }};
 
     final String EXPLANATION_NAMESPACE = "urn:qanary:explanations#";
