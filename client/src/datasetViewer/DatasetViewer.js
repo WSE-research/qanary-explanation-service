209ec958d6f9dbf4c4601adfbf3ebe08b312731a
Dennis Schiese <schiesedennis@googlemail.com>
2023-10-26 09:24:13 +0200

    Edited Data viewer

--- client/src/datasetViewer/DatasetViewer.js
+++ client/src/datasetViewer/DatasetViewer.js
@@ -1,8 +1,10 @@
 import "./DatasetViewer.css";
-import data from "../json.json";
+import data from "../json2.json";
 import { useState } from "react";
 
 export default function DatasetViewer() {
+
+    const [counter, setCounter] = useState(0);
 
     const goldenExplanations = data.explanations.map((datum) => {
         return datum.testData.explanation;
@@ -14,38 +16,14 @@ export default function DatasetViewer() {
 
     const arrayLength = data.explanations.length;
 
-    function decreaseCounter() {
-        if(counter > 0) {
-            setCounter(counter-1);
-            setExplanation();
-        }
-    }
-
-    function increaseCounter() {
-        if(counter < arrayLength) {
-            setCounter(counter+1)
-            console.log(counter)
-            setExplanation();
-        }
-    }
-
-    function setExplanation() {
-        setGoldenExplanation(goldenExplanations[counter])
-        setGptExplanation(gptExplanations[counter])
-    }
-
-    const [goldenExplanation, setGoldenExplanation] = useState("");
-    const [gptExplanation, setGptExplanation] = useState("");
-    const [counter, setCounter] = useState(0);
-
     return(
         <div className="container">
-            {counter}
+            Current Explanation: {counter+1} / {arrayLength}
             <div className="viewerContainer">
                 <div className="viewer">
                     <p style={{ fontSize: 28, top: '10 %'}}>Golden standard</p>
                     <div className="explanation">
-                        <p className="explanationText">{goldenExplanation}</p>
+                        <p className="explanationText">{goldenExplanations[counter]}</p>
                     </div>
                 </div>
             </div>
@@ -53,18 +31,18 @@ export default function DatasetViewer() {
                 <div className="viewer">
                     <p style={{ fontSize: 28, top: '10 %'}}>GPT Explanation</p>
                     <div className="explanation">
-                        <div className="explanationText">{gptExplanation}</div>
+                        <div className="explanationText">{gptExplanations[counter]}</div>
                     </div>
                 </div>
             </div>
             <div className="buttons">
                 <div style={{marginTop: 50}}>
-                    <button onClick={decreaseCounter}>
+                    <button onClick={() => counter > 0 ? setCounter(counter-1) : null}>
                         Vorheriges
                     </button>
                 </div>
                 <div style={{bottom: 0, marginBottom: 50}}>
-                    <button onClick={increaseCounter}>
+                    <button onClick={() => counter < arrayLength ? setCounter(counter+1) : null}>
                         Nachfolgendes
                     </button>
                 </div>
