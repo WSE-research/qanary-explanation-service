13539e7a26c84d818a0517b524eb2a79c2ae6e59
Dennis Schiese <schiesedennis@googlemail.com>
2024-01-09 21:10:27 +0100

    adjusted shell script to support multi-digit versions for qanary commons

--- dockerfile_scripts/extract_commons_version.sh
+++ dockerfile_scripts/extract_commons_version.sh
@@ -1,7 +1,7 @@
 cd target && ls
 #JAR_FILE=$(find . -maxdepth 1 -type f -name "qa.commons-[0-9].[0-9].[0-9].jar")
 #JAR_VERSION=$(echo $JAR_FILE | grep -oP 'qa.commons-\K\d+\.\d+\.\d+')
-JAR_FILE=$(find . -maxdepth 1 -type f -name "qa.commons-[0-9]*.[0-9]*.[0-9]*.jar")
+JAR_FILE=$(find . -maxdepth 1 -type f -name "qa.commons-[0-9]*.[0-9]*.[0-9].jar")
 JAR_VERSION=$(echo $JAR_FILE | grep -oP 'qa.commons-\K\d+\.\d+\.\d+')
 mv qa.commons-$JAR_VERSION.jar qa.commons.jar # Rename commons jar
 cp qa.commons.jar /app
