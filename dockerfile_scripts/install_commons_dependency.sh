897386433d07d88da0753597ed97a10d7649afb8
Dennis Schiese <schiesedennis@googlemail.com>
2024-05-13 22:54:14 +0200

    Removed docker scripts due to commons availability in maven central, removed qa.qanary_component-EntityIdentifier

--- dockerfile_scripts/install_commons_dependency.sh
+++
@@ -1,4 +0,0 @@
-JAR_VERSION=$(cat jar_version | xargs)
-
-# Install commons-dependency
-mvn install:install-file -Dfile=qa.commons.jar -DgroupId=eu.wdaqua.qanary -DartifactId=qa.commons -Dversion=$JAR_VERSION -Dpackaging=jar
