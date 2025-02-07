bc7675d9900443ac9a65a47980fc4703bc4bc7db
Dennis Schiese <dennisschiese@fedora.fritz.box>
2025-02-07 10:55:38 +0100

    Refactor GenerativeExplanationsRepository

---
+++ src/main/java/com/wse/qanaryexplanationservice/exceptions/GenerativeExplanationException.java
@@ -0,0 +1,7 @@
+package com.wse.qanaryexplanationservice.exceptions;
+
+public class GenerativeExplanationException extends RuntimeException {
+    public GenerativeExplanationException(String message) {
+        super(message);
+    }
+}
